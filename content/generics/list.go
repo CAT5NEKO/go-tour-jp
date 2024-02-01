42dd6ec0888b4c0187f83410c8b6a897b2f2437b
CAT5NEKO <JFKnet1984@gmail.com>
2024-02-01 23:10:27 +0900

    Add generics function

---
+++ content/generics/list.go
@@ -0,0 +1,14 @@
+//go:build OMIT
+// +build OMIT
+
+package main
+
+// List represents a singly-linked list that holds
+// values of any type.
+type List[T any] struct {
+	next *List[T]
+	val  T
+}
+
+func main() {
+}
